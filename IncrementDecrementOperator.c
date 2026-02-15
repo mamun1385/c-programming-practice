d197c3c1d76e85287e9082ffd96b13edb1062f13
mamun1385 <mamun113457@gmail.com>
2026-02-15 14:39:27 +0600

    Add files via upload

---
+++ IncrementDecrementOperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int a=10, b=20;
+    float c=12.54, d=34.21;
+
+    printf("++a = %d\n", ++a);
+    printf("--b = %d\n", --b);
+    printf("++c = %d\n", ++c);
+    printf("--d = %d\n", --d);
+
+    return 0;
+}
